45f7ecb66293becc268e702912f4ce0a622c3645
HoAi <art_alys@hotmail.com>
2021-08-17 16:41:49 +0700

    import main as run

--- run_Queen_Bot.py
+++ run_Queen_Bot.py
@@ -1,3 +1,4 @@
-from Queen_Bot import main
+from Queen_Bot import main as run
+use_token = "ODc1NDA1ODU3NDE0ODY0ODk2.YRVDPA.7MrFbQgqLD7iZBLqCyotO_k1Ehc"
 if __name__ == '__main__':
-    main()
+    run(use_token)
