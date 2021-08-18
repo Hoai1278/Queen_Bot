659ca1b3da2cbc19efae03f649d2861597b0d065
HoAi <art_alys@hotmail.com>
2021-08-18 18:18:26 +0700

    ready to goes public!

--- run_Queen_Bot.py
+++ run_Queen_Bot.py
@@ -1,4 +1,6 @@
 from Queen_Bot import main as run
-use_token = "ODc1NDA1ODU3NDE0ODY0ODk2.YRVDPA.7MrFbQgqLD7iZBLqCyotO_k1Ehc"
+from dotenv import load_dotenv
+import os
+load_dotenv()
 if __name__ == '__main__':
-    run(use_token)
+    run(os.getenv("use_token"))
